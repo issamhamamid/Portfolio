4b6ee6622be00703643230b84fb9db3999e407b8
Issam Hammamid <issamhamamid6@gmail.com>
2025-05-08 18:26:01 +0100

    navbar animations

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,15 +4,21 @@ import Links from "./../data/Links";
 
 export const Header = () => {
     return (
-        <header className='bg-[#080e21] w-full py-5 px-2'>
-            <div className='w-320 flex items-center justify-between mx-auto'>
-               <h1 className='text-[#9b87f5] text-2xl font-bold'>ISM.</h1>
-                <nav className='flex gap-8'>
+        <header className='bg-[#080e21] w-full py-5 px-4'>
+            <div className='w-full xl:w-320 flex items-center justify-between mx-auto'>
+               <h1 className='text-[#9b87f5] text-2xl font-extrabold tracking-wide'>ISM.</h1>
+                <nav className=' hidden lg:flex gap-1 '>
                     {
                         Links.map((link) => (
                            <a key={link.name}
                               href={link.url}
-                              className = "text-white text-sm font-medium"
+                              className="px-4 py-1.5 relative
+                                         after:content-[''] after:absolute after:left-0 after:bottom-0
+                                         after:h-[.125rem] after:bg-[#9b87f5] after:w-0
+                                         after:transition-all after:duration-300 after:ease-out
+                                         hover:after:w-full
+                                         text-white text-sm font-medium tracking-wide
+                                         hover:text-[#9b87f5] transition-all duration-500 ease-out"
                            >
                                {link.name}
                            </a>
